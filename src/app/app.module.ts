3478015f71531e9f52c21ad62c14aede00f60129
Johhan Santana <johhan.santana@gmail.com>
2017-10-24 12:20:25 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatCardModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatTabsModule
+} from '@angular/material'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { HomeComponent } from './home/home.component'
+import { RouterModule, Routes } from '@angular/router'
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '/' }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes, {
+        enableTracing: false
+      }
+    ),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTabsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
